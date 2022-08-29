7e349a93f64fa5a762f305623d663b3a60152e2a
ghazal <gh.eslamifard@gmail.com>
2022-08-29 18:15:18 +0430

    install EmotionJS and define a theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
+import { Global, ThemeProvider } from "@emotion/react";
+
+import { globalStyles } from "../styles/global-styles";
+import { theme } from "../styles/theme";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
